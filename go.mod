2deb2c1c1be4edba6540f84fb2a49e4348c04621
Waldemar Quevedo <wally@synadia.com>
2021-09-24 13:23:12 -0700

    Add tests
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wallyqs/go-mod-b
 
 go 1.17
+
+require github.com/wallyqs/go-mod-a v0.0.0-20210924201556-e67af2677460
--- world_test.go
+++ world_test.go
@@ -1,12 +1,22 @@
 package world
 
 import (
+	"fmt"
 	"testing"
+	"github.com/wallyqs/go-mod-a"
 )
 
 func TestWorld(t *testing.T) {
 	got := World()
 	expected := "World"
+	if got != expected {
+		t.Fatalf("Expected %v, got: %v", expected, got)
+	}
+}
+
+func TestHelloWorld(t *testing.T) {
+	got := fmt.Sprintf("%s %s", hello.Hello(), World())
+	expected := "Hello World"
 	if got != expected {
 		t.Fatalf("Expected %v, got: %v", expected, got)
 	}
